5e4124433b0201e35db372079257a92ea6c13b49
Paul Burmester <paulburmester2@gmail.com>
2026-01-01 13:13:24 +0100

    added Endboss

--- models/endboss.class.js
+++ models/endboss.class.js
@@ -1,19 +1,21 @@
-class Chicken extends MovableObject {
+class Endboss extends MovableObject {
+
     y = 360;
     height = 60;
     width = 60;
     IMAGES_WALKING = [
-        "img/3_enemies_chicken/chicken_normal/1_walk/1_w.png",
-        "img/3_enemies_chicken/chicken_normal/1_walk/2_w.png",
-        "img/3_enemies_chicken/chicken_normal/1_walk/3_w.png"
+        "img/4_enemie_boss_chicken/1_walk/G1.png",
+        "img/4_enemie_boss_chicken/1_walk/G2.png",
+        "img/4_enemie_boss_chicken/1_walk/G3.png",
+        "img/4_enemie_boss_chicken/1_walk/G4.png"
     ]
 
     constructor() {
-        super().loadImage("img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+        super().loadImage("img/4_enemie_boss_chicken/1_walk/G1.png");
         this.loadImages(this.IMAGES_WALKING);
 
-        this.x = 200 + Math.random() * 500;
-        this.speed = 0.15 + Math.random() * 0.25;
+        this.x = 200;
+        this.speed = 0.15;
         this.animate();
     }
 
